9212fd7b1cb62074c2c7d1b77caf343657e188e5
Vegan Cat <mmostafavi247@gmail.com>
2022-12-12 03:02:31 +0330

    add update commission

--- programs/unstake-my-sol/src/lib.rs
+++ programs/unstake-my-sol/src/lib.rs
@@ -10,6 +10,16 @@ pub mod unstake_my_sol {
         Ok(())
     }
 
+    pub fn update_commission(ctx: Context<UpdateCommission>, commission: u16) -> Result<()> {
+        let liquidity_acc = &mut ctx.accounts.liquidity_acc;
+        let user = &mut ctx.accounts.user;
+
+        require_keys_eq!(user.key(), liquidity_acc.owner);
+        liquidity_acc.commission = commission;
+
+        Ok(())
+    }
+
     pub fn deposit(ctx: Context<Deposit>) -> Result<()> {
         Ok(())
     }
@@ -28,6 +38,22 @@ pub mod unstake_my_sol {
 }
 
 #[derive(Accounts)]
+pub struct CreateLiquidityAcc<'info> {
+    #[account(init, payer = user, space = 8 + 2 + 32)]
+    pub liquidity_acc: Account<'info, LiquidityAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct UpdateCommission<'info> {
+    #[account(mut)]
+    pub liquidity_acc: Account<'info, LiquidityAccount>,
+    pub user: Signer<'info>,
+}
+
+#[derive(Accounts)]
 pub struct Deposit {}
 
 #[derive(Accounts)]
@@ -39,16 +65,8 @@ pub struct Swap {}
 #[derive(Accounts)]
 pub struct Liquidate {}
 
-#[derive(Accounts)]
-pub struct CreateLiquidityAcc<'info> {
-    #[account(init, payer = user, space = 8 + 2)]
-    pub liquidity_acc: Account<'info, LiquidityAccount>,
-    #[account(mut)]
-    pub user: Signer<'info>,
-    pub system_program: Program<'info, System>,
-}
-
 #[account]
 pub struct LiquidityAccount {
     commission: u16, // 2
+    owner: Pubkey,   // 32
 }
