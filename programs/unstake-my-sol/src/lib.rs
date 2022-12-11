bf20a6c10a8b5112e854db9a7ad941934a524bbf
Vegan Cat <mmostafavi247@gmail.com>
2022-12-12 02:51:25 +0330

    add create liquidity account instruction

--- programs/unstake-my-sol/src/lib.rs
+++ programs/unstake-my-sol/src/lib.rs
@@ -6,6 +6,10 @@ declare_id!("2s5Phqos3TVKh6CyuFqm8vQukzxueJfMWgtci224h9qc");
 pub mod unstake_my_sol {
     use super::*;
 
+    pub fn create_liquidity_acc(ctx: Context<CreateLiquidityAcc>) -> Result<()> {
+        Ok(())
+    }
+
     pub fn deposit(ctx: Context<Deposit>) -> Result<()> {
         Ok(())
     }
@@ -34,3 +38,17 @@ pub struct Swap {}
 
 #[derive(Accounts)]
 pub struct Liquidate {}
+
+#[derive(Accounts)]
+pub struct CreateLiquidityAcc<'info> {
+    #[account(init, payer = user, space = 8 + 2)]
+    pub liquidity_acc: Account<'info, LiquidityAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct LiquidityAccount {
+    commission: u16, // 2
+}
