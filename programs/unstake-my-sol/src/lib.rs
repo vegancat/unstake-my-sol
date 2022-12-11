72f3f804941b2497f723eff3de96029611fe91ff
Vegan Cat <mmostafavi247@gmail.com>
2022-12-12 02:28:39 +0330

    Add basic definition of smart contract

--- programs/unstake-my-sol/src/lib.rs
+++ programs/unstake-my-sol/src/lib.rs
@@ -6,10 +6,31 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod unstake_my_sol {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn deposit(ctx: Context<Deposit>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn swap(ctx: Context<Swap>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn liquidate(ctx: Context<Liquidate>) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Deposit {}
+
+#[derive(Accounts)]
+pub struct Withdraw {}
+
+#[derive(Accounts)]
+pub struct Swap {}
+
+#[derive(Accounts)]
+pub struct Liquidate {}
